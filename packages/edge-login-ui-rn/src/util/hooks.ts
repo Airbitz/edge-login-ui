aded56dcf48be5e82a0e2c63164d27d80ebc85ca
William Swanson <swansontec@gmail.com>
2021-04-29 14:39:58 -0700

    rn: Fix remaining type errors

--- packages/edge-login-ui-rn/src/util/hooks.ts
+++ packages/edge-login-ui-rn/src/util/hooks.ts
@@ -1,67 +1,12 @@
 import * as React from 'react'
 
-type SetState<S> = (value: S | ((state: S) => S)) => void
-
-type UseCallback = <T extends (...args: any[]) => any>(
-  callback: T,
-  deps: any[]
-) => T
-
-type UseContext = <T>(context: React.Context<T>) => T
-
-type UseDebugValue = <T>(value: T, format?: (value: T) => any) => void
-
-type UseEffect = (effect: () => undefined | (() => void), deps?: any[]) => void
-
-type UseImperativeHandle = (ref: any, init: () => any, deps?: any[]) => void
-
-type UseMemo = <T>(init: () => T, deps?: any[]) => T
-
-interface UseReducer {
-  // Normal version:
-  <State, Action>(
-    reducer: (state: State | undefined, action: Action) => State,
-    init: State | undefined
-  ): [State, (action: Action) => void]
-
-  // Initializer version:
-  <State, Action, Init>(
-    reducer: (state: State | undefined, action: Action) => State,
-    init: Init,
-    initializer: (init: Init) => State
-  ): [State, (action: Action) => void]
-}
-
-interface UseRef {
-  // Component ref:
-  <ElementType extends React$ElementType>(): {
-    current: null | React$ElementRef<ElementType>
-  }
-
-  // Value container:
-  <T>(init: T): { current: T }
-}
-
-type UseState = <S>(init: S | (() => S)) => [S, SetState<S>]
-
-// $FlowFixMe
-export const useCallback: UseCallback = React.useCallback
-// $FlowFixMe
-export const useContext: UseContext = React.useContext
-// $FlowFixMe
-export const useDebugValue: UseDebugValue = React.useDebugValue
-// $FlowFixMe
-export const useEffect: UseEffect = React.useEffect
-export const useImperativeHandle: UseImperativeHandle =
-  // $FlowFixMe
-  React.useImperativeHandle
-// $FlowFixMe
-export const useLayoutEffect: UseEffect = React.useLayoutEffect
-// $FlowFixMe
-export const useMemo: UseMemo = React.useMemo
-// $FlowFixMe
-export const useReducer: UseReducer = React.useReducer
-// $FlowFixMe
-export const useRef: UseRef = React.useRef
-// $FlowFixMe
-export const useState: UseState = React.useState
+export const useCallback = React.useCallback
+export const useContext = React.useContext
+export const useDebugValue = React.useDebugValue
+export const useEffect = React.useEffect
+export const useImperativeHandle = React.useImperativeHandle
+export const useLayoutEffect = React.useLayoutEffect
+export const useMemo = React.useMemo
+export const useReducer = React.useReducer
+export const useRef = React.useRef
+export const useState = React.useState
